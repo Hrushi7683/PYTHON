e0851708efe4878c1394e7bec4147c483c788e25
Hrushikesh Sunil Kadam <hrushikeshkdm7683@gmail.com>
2023-05-08 19:55:40 +0530

    enumerate commit

---
+++ ennumerate.py
@@ -0,0 +1,12 @@
+for i , val in enumerate(list[1,2,3,4,5,6,7,8,9]) :
+    print(i , val)
+
+#So enumerate is use to get the index of list
+
+
+for i , val in enumerate(range(0,20)) :
+    print(i , val)
+
+
+for i , val in enumerate(list[range(30 , 0 , -3)]) :
+   print(i , val)    
